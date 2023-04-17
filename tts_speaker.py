75c2e579f468153020c7e2dc1bb68c3f00e56707
unknown <john.eicher89@gmail.com>
2023-04-17 14:38:26 -0700

    Upload tts_speaker.py

---
+++ tts_speaker.py
@@ -0,0 +1,18 @@
+from gtts import gTTS
+from io import BytesIO
+from pydub import AudioSegment
+from pydub.playback import play
+from elevenlabslib import *
+
+def say_gTTS(text, lang = "en"):
+    tts = gTTS(text=text, lang=lang)
+    fp = BytesIO()
+    tts.write_to_fp(fp)
+    fp.seek(0)
+    song = AudioSegment.from_file(fp, format="mp3")
+    play(song)
+
+def say_elvenlabs(text):
+    user = ElevenLabsUser("API_KEY")
+    voice = user.get_voices_by_name("Rachel")[0]
+    voice.generate_and_play_audio(text, playInBackground=False)
