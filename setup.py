98aec9e47a16acd81ac4325ea52a4a480e4089f3
john <john.eicher89@gmail.com>
2023-04-23 15:45:34 -0700

    Fixing pip install

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
 
 setuptools.setup(
     name='tts_helper',
@@ -9,13 +9,13 @@ setuptools.setup(
     author='John Eicher',
     author_email='john.eicher89@gmail.com',
     description='Testing installation of Package',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
     url='https://github.com/saltchicken/tts_helper',
-    project_urls = {
-        "Bug Tracker": "https://github.com/saltchicken/tts_helper/issues"
-    },
-    license='MIT',
-    packages=['tts_helper'],
+    # project_urls = {
+    #     "Bug Tracker": "https://github.com/saltchicken/tts_helper/issues"
+    # },
+    # license='MIT',
+    py_modules=['tts_helper'],
     install_requires=['gtts', 'pydub', 'elevenlabs'],
 )
